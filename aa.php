1f722ad11e7a0e03d379afe3360a6864a8a545b2
piaodong <44664040+piaodong@users.noreply.github.com>
2018-11-14 18:41:15 +0800

    Delete aa.php

--- aa.php
+++
@@ -1,9 +0,0 @@
-<?php 
- $img = $_FILES['Article'];
-            $tmp_name = $img['tmp_name']['articleimg'];
-            $img_path = 'upload/'.$img['name']['articleimg'];
-    
-            move_uploaded_file($tmp_name,$img_path);  //临时文件名称   目的地
-
-
-?>
